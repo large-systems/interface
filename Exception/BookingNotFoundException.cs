84e0764da8a5436af19915be8ae1d32131bb15ec
cphjs <cph-js408@cphbusiness.dk>
2019-10-27 09:55:53 -0700

    Add data contract attributes to exceptions, increment version.

--- Exception/BookingNotFoundException.cs
+++ Exception/BookingNotFoundException.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Runtime.Serialization;
 
 namespace HotelSystem.Exception
 {
+    [DataContract]
     public class BookingNotFoundException : System.Exception
     {
     }
